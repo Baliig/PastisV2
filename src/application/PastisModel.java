bd2634756f442072f7c731425ca7d6dab5f6a614
Jeremy Portelli <jeremyp.1994@hotmail.com>
2016-05-26 17:47:28 +0200

    v1.5

--- src/application/PastisModel.java
+++ src/application/PastisModel.java
@@ -8,13 +8,16 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class PastisModel {
-	
-	String  UC_CONSONANTS = "BCDFGHJKLMNPQRSTVWXZ";
-	String  LC_CONSONANTS = "bcdfghjklmnpqrstvwxz";
-	String  UC_VOWELS     = "AEIOUY";
-	String  LC_VOWELS     = "aeiouy";
-	String  NUMBER		  = "0123456789";
-	String  SYMBOLS       = "@$#%-.!&,:?<>[]/(){}+_^~";
+
+	String UC_CONSONANTS = "BCDFGHJKLMNPQRSTVWXZ";
+	String LC_CONSONANTS = "bcdfghjklmnpqrstvwxz";
+	String UC_CONSONANTS_NOT_AMBIGU = "BCDFGHJKLMNPQRSTVWXZAEUY";
+	String LC_CONSONANTS_NOT_AMBIGU = "bcdfghjkmnpqrstvwxzaeiuy";
+	String NUMBER_NOT_AMBIGU = "123456789";
+	String UC_VOWELS = "AEIOUY";
+	String LC_VOWELS = "aeiouy";
+	String NUMBER = "0123456789";
+	String SYMBOLS = "@$#%-.!&,:?<>[]/(){}+_^~";
 
 	private static final int LENGTH_PASSWORD = 12;
 
@@ -38,40 +41,78 @@ public class PastisModel {
 	public String generateNewPassword() {
 		String passwordGen = "";
 		String list = null;
-		if(lengthPass.get().isEmpty()){
+		String list2 = null;
+		if (lengthPass.get().isEmpty()) {
 			lengthPass.set("12");
 		}
 		int passwordLength = Integer.valueOf(lengthPass.get());
-		
-		if(pronounceable.get()){
-			
-		}
-		
-		if(ambigus.get()){
-			
-		}
-		if(ucLetter.get() || lcLetter.get() || digits.get() || symbole.get()){
-			if(ucLetter.get() == true){
+
+		if (pronounceable.get()) {
+			if (ucLetter.get() == true) {
 				list += UC_CONSONANTS;
-				list += UC_VOWELS;
+				list2 += UC_VOWELS;
 			}
-			if(lcLetter.get()){
+			if (lcLetter.get() == true) {
 				list += LC_CONSONANTS;
-				list += LC_VOWELS;
+				list2 += LC_VOWELS;
 			}
-			if(digits.get()){
-				list += NUMBER;
+			for (int j = 0; j < passwordLength; j++) {
+				Random rdm = new Random();
+				int nb = 0;
+				if(j%2 == 0){
+					nb = rdm.nextInt(list.length());
+					passwordGen += list.charAt(nb);
+				}else{
+					nb = rdm.nextInt(list2.length());
+					passwordGen += list2.charAt(nb);
+				}
+				
 			}
-			if(symbole.get()){
+			return passwordGen;
+		}
+
+		if (ambigus.get()) {
+			if (ucLetter.get() == true) {
+				list += UC_CONSONANTS_NOT_AMBIGU;
+			}
+			if (lcLetter.get()) {
+				list += LC_CONSONANTS_NOT_AMBIGU;
+			}
+			if (digits.get()) {
+				list += NUMBER_NOT_AMBIGU;
+			}
+			if (symbole.get()) {
 				list += SYMBOLS;
 			}
 			for (int i = 0; i < passwordLength; i++) {
 				Random rdm = new Random();
 				int nb = rdm.nextInt(list.length());
-				passwordGen += list.charAt(nb); 
+				passwordGen += list.charAt(nb);
+			}
+			return passwordGen;
+		}
+		if (ucLetter.get() || lcLetter.get() || digits.get() || symbole.get()) {
+			if (ucLetter.get() == true) {
+				list += UC_CONSONANTS;
+				list += UC_VOWELS;
+			}
+			if (lcLetter.get()) {
+				list += LC_CONSONANTS;
+				list += LC_VOWELS;
+			}
+			if (digits.get()) {
+				list += NUMBER;
+			}
+			if (symbole.get()) {
+				list += SYMBOLS;
+			}
+			for (int i = 0; i < passwordLength; i++) {
+				Random rdm = new Random();
+				int nb = rdm.nextInt(list.length());
+				passwordGen += list.charAt(nb);
 			}
 		}
-		
+
 		return passwordGen;
 	}
 
