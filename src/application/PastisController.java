6c7b6856e75d43dc9f667a52cb4dbb7800d413f6
Lucas <Lucas.8994@hotmail.com>
2016-05-27 08:50:43 +0200

    v2

--- src/application/PastisController.java
+++ src/application/PastisController.java
@@ -3,6 +3,7 @@ package application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
